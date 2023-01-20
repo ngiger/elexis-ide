d4e7293878571cd2ae240418dc608bbdf5d8a44c
Niklaus Giger <niklaus.giger@member.fsf.org>
2023-01-20 16:50:06 +0100

    Setup 22.09. git clone does not yet work

--- setup_elexis.sh
+++ setup_elexis.sh
@@ -1,34 +1,26 @@
 #!/usr/bin/env bash
-set -o verbose
+#set -o verbose
+set -o pipefail
+set -e
 SCRIPT=$(readlink -f $0)
 SCRIPTPATH=`dirname $SCRIPT`
-echo SCRIPTPATH ist $SCRIPTPATH
 ELEXIS_INSTALLER=$SCRIPTPATH/eclipse-installer/eclipse-inst
 if  test -f $ELEXIS_INSTALLER; then
-echo ELEXIS_INSTALLER found as $ELEXIS_INSTALLER
+  echo ELEXIS_INSTALLER found as $ELEXIS_INSTALLER
 else
-echo NO ELEXIS_INSTALLER found as $ELEXIS_INSTALLER, fetching it
-mvn org.apache.maven.plugins:maven-dependency-plugin:3.3.0:unpack \
--Dartifact=com.github.a-langer:org.eclipse.oomph.console.product:1.0.2:tar.gz:linux.gtk.x86_64  \
--DoutputDirectory=./ -Dproject.basedir=./
-ls -l $ELEXIS_INSTALLER
+  echo NO ELEXIS_INSTALLER found as $ELEXIS_INSTALLER, fetching it
+  mvn org.apache.maven.plugins:maven-dependency-plugin:3.3.0:unpack \
+  -Dartifact=com.github.a-langer:org.eclipse.oomph.console.product:1.0.2:tar.gz:linux.gtk.x86_64  \
+  -DoutputDirectory=./ -Dproject.basedir=./
 fi
 
 SETUPS=$SCRIPTPATH/elexis/
-echo PWD ist $PWD
 INST_ROOT=/tmp/elexis_ide2
 rm -rf $INST_ROOT; mkdir -p $INST_ROOT
-echo PWD ist $PWD
-ls -l $ELEXIS_INSTALLER $SETUPS
-cd $SCRIPTPATH/eclipse-installer
 INST_ROOT=/tmp/elexis-ide
 rm -rf $INST_ROOT
-echo PWD ist $PWD
-# INST=$SCRIPTPATH/./eclipse-inst
-# INST=$SCRIPTPATH/org.eclipse.oomph.console.product/target/products/org.eclipse.oomph.console.product/linux/gtk/x86_64/eclipse-installer/eclipse-inst
-time $ELEXIS_INSTALLER -nosplash -application org.eclipse.oomph.console.application -vmargs \
+$ELEXIS_INSTALLER -nosplash -application org.eclipse.oomph.console.application -vmargs \
   -Doomph.redirection.setups="index:/->$SETUPS/" \
-  -Doomph.redirection.setupsDir="index:/->$SETUPS/" \
   -Doomph.installation.location="$INST_ROOT/ide" \
   -Doomph.product.id="eclipse.ide4elexis" \
   -Doomph.project.id="elexis.ide" \
@@ -36,11 +28,11 @@ time $ELEXIS_INSTALLER -nosplash -application org.eclipse.oomph.console.applicat
   -Doomph.installation.id="elexis.ide2" \
   -Dsetup.p2.agent="$HOME/.p2" \
   -Doomph.setup.offline=false \
-  -Doomph.installer.verbose=true 2>&1 | tee  $0.log
-du -shx $INST_ROOT
+  -Declipse.train=2022-09 \
+  -Dcore.git.clone.location=git@github.com:elexis/elexis-3-core.git \
+  -Dgithub.remoteURIs=git@github.com:elexis/ \
+  -Dgit.container.root="$INST_ROOT/git" \
+  -Doomph.installer.verbose=true 2>&1 | tee $SCRIPTPATH/$0.log
 IDE=`find $INST_ROOT -name eclipse`
 echo SETUPS were in $SETUPS IDE is $IDE
-export START=" $IDE -data $INST_ROOT/workspace -clean"
-echo $START
-$START
-exit
+echo run it via: $IDE -data $INST_ROOT/workspace -clean
